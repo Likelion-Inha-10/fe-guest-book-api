1cd44ea3fb70de096b6b3c5ecd803a72faf2194a
devHudi <devhudi@gmail.com>
2022-06-16 16:26:52 +0900

    feat: swagger 추가

--- src/main/java/likelion/guestbook/controller/ArticleController.java
+++ src/main/java/likelion/guestbook/controller/ArticleController.java
@@ -1,5 +1,7 @@
 package likelion.guestbook.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import java.net.URI;
 import java.util.List;
 import likelion.guestbook.domain.OwnerId;
@@ -7,15 +9,16 @@ import likelion.guestbook.dto.ArticleRequestDto;
 import likelion.guestbook.dto.ArticleResponseDto;
 import likelion.guestbook.service.ArticleService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@Api(value = "방명록", tags = "이미지와 이미지의 댓글을 관리합니다.")
+@RestController
 public class ArticleController {
     private final ArticleService articleService;
 
@@ -23,6 +26,7 @@ public class ArticleController {
         this.articleService = articleService;
     }
 
+    @Operation(summary = "특정 유저에 방명록 생성", description = "특정 유저에 대한 방명록을 생성합니다.")
     @PostMapping("/{ownerId}/articles")
     public ResponseEntity<ArticleResponseDto> createArticle(@PathVariable String ownerId,
                                                             @RequestBody ArticleRequestDto articleRequestDto) {
@@ -30,18 +34,21 @@ public class ArticleController {
         return ResponseEntity.created(URI.create("/" + ownerId + "/articles/" + id)).build();
     }
 
+    @Operation(summary = "특정 유저 방명록 목록 조회", description = "특정 유저의 이름을 전달하여 유저의 방명록을 모두 조회합니다.")
     @GetMapping("/{ownerId}/articles")
     public ResponseEntity<List<ArticleResponseDto>> findAllByOwnerId(@PathVariable String ownerId) {
         List<ArticleResponseDto> articleResponseDtos = articleService.findAllByOwnerId(new OwnerId(ownerId));
         return ResponseEntity.ok(articleResponseDtos);
     }
 
+    @Operation(summary = "특정 방명록 조회", description = "방명록을 ID로 단건 조회합니다.")
     @GetMapping("/articles/{id}")
     public ResponseEntity<ArticleResponseDto> findById(@PathVariable String id) {
         ArticleResponseDto articleResponseDto = articleService.findById(Long.valueOf(id));
         return ResponseEntity.ok(articleResponseDto);
     }
 
+    @Operation(summary = "방명록 수정", description = "방명록을 수정합니다.")
     @PutMapping("/articles/{id}")
     public ResponseEntity<Void> updateArticle(@PathVariable String id,
                                               @RequestBody ArticleRequestDto articleRequestDto) {
@@ -49,6 +56,7 @@ public class ArticleController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "방명록 제거", description = "방명록을 제거합니다.")
     @DeleteMapping("/articles/{id}")
     public ResponseEntity<Void> deleteArticle(@PathVariable String id) {
         articleService.deleteArticle(Long.valueOf(id));
